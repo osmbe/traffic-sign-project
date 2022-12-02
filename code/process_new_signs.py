bd665d2f9b4c9b85f2e031ec160b7c27a694a687
Ivan <feleir@gmail.com>
2022-12-02 09:24:15 +0100

    Update api key for testing purposes in github.

--- code/process_new_signs.py
+++ code/process_new_signs.py
@@ -15,7 +15,7 @@ maproulette_api_key = environ.get("MAPROULETTE_API_KEY")
 challenge_id = environ['CHALLENGE_ID']
 
 if __name__== "__main__":
-  logging.info("Processing new features after %s, updates will be published to challenge %s", process_date, challenge_id)
+  logging.info("Processing new features after %s, updates will be published to challenge %s [API key: %s].", process_date, challenge_id, maproulette_api_key)
   fetch_all_features_by_type(wfs_url, feature_file, feature_type)
   signs_dataframe = extract_new_signs(feature_file, process_date)
   if signs_dataframe.empty:
