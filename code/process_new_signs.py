f4df5fb25a64df16ab10865144b39ac95922cc3c
Feleir <feleir@gmail.com>
2023-08-05 09:57:25 +0200

    Revert testing changes

--- code/process_new_signs.py
+++ code/process_new_signs.py
@@ -11,15 +11,15 @@ feature_type = "awv:Verkeersborden.Vlaanderen_Borden"
 feature_file = "./python_output/feature_output.csv"
 geojson_file = "./python_output/geojson_output.json"
 process_date = get_process_date()
-#maproulette_api_key = environ.get("MAPROULETTE_API_KEY")
-#challenge_id = environ['CHALLENGE_ID']
+maproulette_api_key = environ.get("MAPROULETTE_API_KEY")
+challenge_id = environ['CHALLENGE_ID']
 
 if __name__== "__main__":
-  logging.info("Processing new features after %s, updates will be published.", process_date)
+  logging.info("Processing new features after %s, updates will be published to challenge %s [API key: %s].", process_date, challenge_id, maproulette_api_key)
   fetch_all_features_by_type(wfs_url, feature_file, feature_type)
   signs_dataframe = extract_new_signs(feature_file, process_date)
   if signs_dataframe.empty:
     logging.info("No new features found.")
   else:
     save_geojson(signs_dataframe, geojson_file)
-    #upload_to_maproulette(maproulette_api_key, challenge_id, geojson_file)
+    upload_to_maproulette(maproulette_api_key, challenge_id, geojson_file)
